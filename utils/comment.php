d04ea9532d38aea01d2b5ff5a2fefe19eecf544a
tbailleu <tbailleu@users.noreply.github.com>
2018-10-08 22:46:17 +0200

    [SCRIPT] like and comment

---
+++ utils/comment.php
@@ -0,0 +1,23 @@
+<?php
+require_once("database.php");
+
+if (!(array_key_exists('user', $_SESSION))) {echo "User not logged"; die();}
+
+$data = json_decode($_REQUEST["json"], true);
+
+if (count($data) != 2) {echo "Invalid number of arguments needed (2) but received (".count($data).")"; die();}
+if (!array_key_exists('imageid', $data) || !strlen($data['imageid'])) {echo "image ID not found"; die();}
+if (!array_key_exists('message', $data) || !strlen($data['message']) || !strlen(trim($data['message']))) {echo "message field not found"; die();}
+
+$data['message'] = trim($data['message']);
+
+if (!preg_match("/^[a-zA-Z0-9 ]{1,250}$/", $data['message'])) {echo "Message field mal-formed"; die();}
+
+$comment = $pdo->prepare("INSERT INTO `comment` (`text`, `user_id`, `image_id`) VALUES (:msg, :userid, :imageid)");
+$comment->execute(array(
+    'msg' => $data['message'],
+    'userid' => $_SESSION['user']['id'],
+    'imageid' => $data['imageid']
+));
+
+echo $data['message'];
---
+++ utils/like.php
@@ -0,0 +1,24 @@
+<?php
+require_once("database.php");
+
+if (!(array_key_exists('user', $_SESSION))) {echo "User not logged"; die();}
+
+$data = json_decode($_REQUEST["json"], true);
+
+if (count($data) != 1) {echo "Invalid number of arguments needed (1) but received (".count($data).")"; die();}
+if (!array_key_exists('imageid', $data) || !strlen($data['imageid'])) {echo "image ID not found"; die();}
+
+$nblike = $pdo->prepare("SELECT `nblike` FROM `image` WHERE `id`=:imageid");
+if (!($nblike->execute(array('imageid' => intval($data['imageid']))))){
+    echo "Invalid imageid"; die();
+};
+$nblike = intval($nblike->fetch());
+
+if ($nblike < 2147483645)
+    $nblike++;
+else { echo "Well done, you won this game !!!"; die(); }
+
+$update = $pdo->prepare("UPDATE `image` SET `nblike`=:nblike WHERE `id`=:imageid");
+$update->execute(array('nblike' => $nblike, 'imageid' => intval($data['imageid'])));
+
+echo $nblike;
